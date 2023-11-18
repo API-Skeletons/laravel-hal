4c8d557674f0077704ba42a5f53335bbc5a2a686
Tom H Anderson <tom.h.anderson@gmail.com>
2023-11-17 18:18:06 -0700

    Configured to sizzle

---
+++ conf.py
@@ -0,0 +1,10 @@
+project = 'laravel/hal'
+author = 'Tom H Anderson <tom.h.anderson@gmail.com>'
+copyright = '2023 API Skeletons <contact@apiskeletons.com>'
+master_doc = 'index'
+show_sphinx = True
+commit = True
+last_updated = True
+pygments_style = 'sphinx'
+html_theme = 'sizzle'
+html_favicon = 'favicon.ico'
